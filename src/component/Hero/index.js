2850c03fc9d8cabee57332ef5fb6e5fe9249b8ff
unknown <s22110390@student.unklab.ac.id>
2024-11-06 18:06:16 -0800

    cantika

---
+++ src/component/Hero/index.js
@@ -0,0 +1,530 @@
+<main class="site-content">
+<section
+  class="hero section center-content illustration-section-01"
+>
+  <div class="container-sm">
+    <div class="hero-inner section-inner">
+      <div class="hero-content">
+        <h1
+          class="mt-0 mb-16 reveal-from-bottom"
+          data-reveal-delay="200"
+        >
+          Landing template for startups
+        </h1>
+        <div class="container-xs">
+          <p
+            class="mt-0 mb-32 reveal-from-bottom"
+            data-reveal-delay="400"
+          >
+            Our landing page template works on all
+            devices, so you only have to set it up
+            once, and get beautiful results forever.
+          </p>
+        </div>
+      </div>
+      <div
+        class="
+          hero-figure
+          reveal-from-bottom
+          illustration-element-01
+        "
+        data-reveal-value="20px"
+        data-reveal-delay="800"
+      >
+        <img
+          class="has-shadow"
+          src="images/hero-image.png"
+          alt="Hero image"
+          width="896"
+          height="504"
+        />
+      </div>
+    </div>
+  </div>
+</section>
+<section class="clients section reveal-fade">
+  <div class="container">
+    <div
+      class="
+        clients-inner
+        section-inner
+        has-top-divider has-bottom-divider
+      "
+    >
+      <ul class="list-reset">
+        <li>
+          <img
+            src="images/clients-01.svg"
+            alt="Clients 01"
+          />
+        </li>
+        <li>
+          <img
+            src="images/clients-02.svg"
+            alt="Clients 02"
+          />
+        </li>
+        <li>
+          <img
+            src="images/clients-03.svg"
+            alt="Clients 03"
+          />
+        </li>
+        <li>
+          <img
+            src="images/clients-04.svg"
+            alt="Clients 04"
+          />
+        </li>
+        <li>
+          <img
+            src="images/clients-05.svg"
+            alt="Clients 05"
+          />
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+<section class="features-tiles section">
+  <div class="container">
+    <div class="features-tiles-inner section-inner">
+      <div class="tiles-wrap">
+        <div class="tiles-item reveal-from-bottom">
+          <div class="tiles-item-inner">
+            <div class="features-tiles-item-header">
+              <div
+                class="
+                  features-tiles-item-image
+                  mb-16
+                "
+              >
+                <img
+                  src="images/feature-tile-icon-01.svg"
+                  alt="Feature tile icon 01"
+                  width="64"
+                  height="64"
+                />
+              </div>
+            </div>
+            <div
+              class="features-tiles-item-content"
+            >
+              <h4 class="mt-0 mb-8">
+                Join the system
+              </h4>
+              <p class="m-0 text-sm">
+                A pseudo-Latin text used in web
+                design, layout, and printing in
+                place of things to emphasise
+                design.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          class="tiles-item reveal-from-bottom"
+          data-reveal-delay="200"
+        >
+          <div class="tiles-item-inner">
+            <div class="features-tiles-item-header">
+              <div
+                class="
+                  features-tiles-item-image
+                  mb-16
+                "
+              >
+                <img
+                  src="images/feature-tile-icon-02.svg"
+                  alt="Feature tile icon 02"
+                  width="64"
+                  height="64"
+                />
+              </div>
+            </div>
+            <div
+              class="features-tiles-item-content"
+            >
+              <h4 class="mt-0 mb-8">
+                Join the system
+              </h4>
+              <p class="m-0 text-sm">
+                A pseudo-Latin text used in web
+                design, layout, and printing in
+                place of things to emphasise
+                design.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          class="tiles-item reveal-from-bottom"
+          data-reveal-delay="400"
+        >
+          <div class="tiles-item-inner">
+            <div class="features-tiles-item-header">
+              <div
+                class="
+                  features-tiles-item-image
+                  mb-16
+                "
+              >
+                <img
+                  src="images/feature-tile-icon-03.svg"
+                  alt="Feature tile icon 03"
+                  width="64"
+                  height="64"
+                />
+              </div>
+            </div>
+            <div
+              class="features-tiles-item-content"
+            >
+              <h4 class="mt-0 mb-8">
+                Join the system
+              </h4>
+              <p class="m-0 text-sm">
+                A pseudo-Latin text used in web
+                design, layout, and printing in
+                place of things to emphasise
+                design.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="pricing section">
+  <div class="container">
+    <div
+      class="pricing-inner section-inner has-top-divider"
+    >
+      <div class="section-header center-content">
+        <div class="container-xs">
+          <h2 class="mt-0 mb-16">
+            Simple, transarent pricing
+          </h2>
+          <p class="m-0">
+            Lorem ipsum is common placeholder text
+            used to demonstrate the graphic elements
+            of a document or visual presentation.
+          </p>
+        </div>
+      </div>
+      <div class="tiles-wrap">
+        <div class="tiles-item reveal-from-bottom">
+          <div class="tiles-item-inner has-shadow">
+            <div class="pricing-item-content">
+              <div
+                class="
+                  pricing-item-header
+                  pb-24
+                  mb-24
+                "
+              >
+                <div
+                  class="
+                    pricing-item-price
+                    mb-4
+                  "
+                >
+                  <span
+                    class="
+                      pricing-item-price-currency
+                      h2
+                    "
+                    >$</span
+                  ><span
+                    class="
+                      pricing-item-price-amount
+                      h1
+                      pricing-switchable
+                    "
+                    data-pricing-monthly="34"
+                    data-pricing-yearly="27"
+                    >27</span
+                  >
+                </div>
+                <div
+                  class="
+                    text-xs text-color-low
+                  "
+                >
+                  Lorem ipsum is a common text
+                </div>
+              </div>
+              <div
+                class="
+                  pricing-item-features
+                  mb-40
+                "
+              >
+                <div
+                  class="
+                    pricing-item-features-title
+                    h6
+                    text-xs text-color-high
+                    mb-24
+                  "
+                >
+                  What’s included
+                </div>
+                <ul
+                  class="
+                    pricing-item-features-list
+                    list-reset
+                    text-xs
+                    mb-32
+                  "
+                >
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li>
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li>
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="pricing-item-cta mb-8">
+              <a
+                class="
+                  button
+                  button-primary
+                  button-block
+                "
+                href="#"
+                >Start free trial</a
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          class="tiles-item reveal-from-bottom"
+          data-reveal-delay="200"
+        >
+          <div class="tiles-item-inner has-shadow">
+            <div class="pricing-item-content">
+              <div
+                class="
+                  pricing-item-header
+                  pb-24
+                  mb-24
+                "
+              >
+                <div
+                  class="
+                    pricing-item-price
+                    mb-4
+                  "
+                >
+                  <span
+                    class="
+                      pricing-item-price-currency
+                      h2
+                    "
+                    >$</span
+                  ><span
+                    class="
+                      pricing-item-price-amount
+                      h1
+                      pricing-switchable
+                    "
+                    data-pricing-monthly="54"
+                    data-pricing-yearly="47"
+                    >47</span
+                  >
+                </div>
+                <div
+                  class="
+                    text-xs text-color-low
+                  "
+                >
+                  Lorem ipsum is a common text
+                </div>
+              </div>
+              <div
+                class="
+                  pricing-item-features
+                  mb-40
+                "
+              >
+                <div
+                  class="
+                    pricing-item-features-title
+                    h6
+                    text-xs text-color-high
+                    mb-24
+                  "
+                >
+                  What’s included
+                </div>
+                <ul
+                  class="
+                    pricing-item-features-list
+                    list-reset
+                    text-xs
+                    mb-32
+                  "
+                >
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li>
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="pricing-item-cta mb-8">
+              <a
+                class="
+                  button
+                  button-primary
+                  button-block
+                "
+                href="#"
+                >Start free trial</a
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          class="tiles-item reveal-from-bottom"
+          data-reveal-delay="400"
+        >
+          <div class="tiles-item-inner has-shadow">
+            <div class="pricing-item-content">
+              <div
+                class="
+                  pricing-item-header
+                  pb-24
+                  mb-24
+                "
+              >
+                <div
+                  class="
+                    pricing-item-price
+                    mb-4
+                  "
+                >
+                  <span
+                    class="
+                      pricing-item-price-currency
+                      h2
+                    "
+                    >$</span
+                  ><span
+                    class="
+                      pricing-item-price-amount
+                      h1
+                      pricing-switchable
+                    "
+                    data-pricing-monthly="74"
+                    data-pricing-yearly="67"
+                    >67</span
+                  >
+                </div>
+                <div
+                  class="
+                    text-xs text-color-low
+                  "
+                >
+                  Lorem ipsum is a common text
+                </div>
+              </div>
+              <div
+                class="
+                  pricing-item-features
+                  mb-40
+                "
+              >
+                <div
+                  class="
+                    pricing-item-features-title
+                    h6
+                    text-xs text-color-high
+                    mb-24
+                  "
+                >
+                  What’s included
+                </div>
+                <ul
+                  class="
+                    pricing-item-features-list
+                    list-reset
+                    text-xs
+                    mb-32
+                  "
+                >
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                  <li class="is-checked">
+                    Excepteur sint occaecat
+                    velit
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="pricing-item-cta mb-8">
+              <a
+                class="
+                  button
+                  button-primary
+                  button-block
+                "
+                href="#"
+                >Start free trial</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</main>
